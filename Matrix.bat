c7b768a55b667406d6c3e2d07568372ef97df37b
Bumblebee99YT <71513684+Bumblebee99YT@users.noreply.github.com>
2021-02-08 14:48:05 -0600

    added Matrix.bat via file upload

---
+++ Matrix.bat
@@ -0,0 +1,12 @@
+@echo off
+
+pause
+
+color 0a
+
+mode 1000
+
+:a
+
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto a
